7aff7c8326d76670d2a8499a3636ad69cb53e87a
Emman Roy Pielago <emmanroy.pielago@cit.edu>
2026-09-25 17:24:04 +0800

    init: vite react app

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+
+    const name = 'Peter'
+  const age = 10
+
+  console.log('Hello from component')
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a + b)
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+    {/*
+      <Hello />
+      <Hello />
+      <Hello />
+      <p>Hello world, it is {now.toString()}</p>
+      <p>{a} plus {b} is {a + b}</p>
+      */}
+      
+      
+     
+       <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+     <Footer />
+   
+    </div>
+  )
+}
+
+export default App
